4ce4ef2d563bc3161db6e211452eb728d30b91de
lachooProject <lachooproject2025@gmail.com>
2025-04-27 11:36:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const AppError = require('./utils/AppError');
+const AppError = require('./utils/apperror');
 const express = require("express");
 const morgan = require("morgan");
 const globalErrorHandler = require("./controllers/errorController");
